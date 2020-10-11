ca0a7690dc61b25e8813131efe1b32f9acce082b
richardsuhendra <richardsuhendra@users.noreply.github.com>
2020-10-10 21:34:28 -0600

    Update hw4_test1.py

--- hw4_test1.py
+++ hw4_test1.py
@@ -1,9 +1,18 @@
-
 import pytest
 import numpy as np
+
 import Week4.field as field
 import Week4.spatial as spatial
 import Week4.timesteppers as timesteppers
+# import field
+# import spatial
+# import timesteppers
+
+# I put my .python files in folders corresponding to the week, so
+# don't forget to change that back to whatever you have it to
+
+# CHANGES FROM ORIGINAL: Added tests for BFD with non-uniform timesteps. Again, error bound is mostly
+# from the original tests, but it should tell you (mostly) if you're doing it right or not
 
 resolution_list = [100, 200, 400]
 
@@ -154,7 +163,7 @@ def test_BDF6_wave(resolution):
     assert error < error_est
 
 
-# test segment by Richard or something. hi addison :)
+# Tests by Richard, don't be sad if they don't pass :(
 
 
 error_nu_BDF2_diff = {(100,2):2e-3, (200,2):5e-4, (400,2):1.5e-4, (100,4):4e-4, (200,4):1e-4, (400,4):2e-5}
@@ -174,11 +183,37 @@ def test_nu_BDF2_diff(resolution, spatial_order):
     ts = timesteppers.BackwardDifferentiationFormula(u, d, 2)
 
     alpha = 0.5
-    ts.evolve(1, alpha*grid.dx)
+    ts.evolve(2, alpha*grid.dx)
     ts.evolve(4, alpha * grid.dx/2)
 
     error = np.max(np.abs( u.data - target))
     error_est = error_nu_BDF2_diff[(resolution,spatial_order)]
+
+    assert error < error_est
+
+error_nu_BDF4_diff = {(100,4):2e-4, (200,4):4e-5, (400,4):1e-5, (100,6):2e-4, (200,6):4e-5, (400,6):1e-5}
+@pytest.mark.parametrize('resolution', resolution_list)
+@pytest.mark.parametrize('spatial_order', [4, 6])
+def test_nu_BDF4_diff(resolution, spatial_order):
+    grid = field.UniformPeriodicGrid(resolution, 50)
+    x = grid.values
+
+    IC = np.exp(-(x-30)**2/4)
+    u = field.Field(grid, IC)
+
+    target = 1/np.sqrt(5)*np.exp(-(x-30)**2/20)
+
+    d = spatial.FiniteDifferenceUniformGrid(2, spatial_order, grid, stencil_type='centered')
+
+    ts = timesteppers.BackwardDifferentiationFormula(u, d, 4)
+
+    alpha = 0.2
+    ts.evolve(1, alpha * grid.dx/2)
+    ts.evolve(3, alpha * grid.dx / 3)
+    ts.evolve(4, alpha*grid.dx)
+
+    error = np.max(np.abs( u.data - target))
+    error_est = error_nu_BDF4_diff[(resolution,spatial_order)]
 
     assert error < error_est
 
